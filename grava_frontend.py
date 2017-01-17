44dfcdc085c039b9cc2c24c5c00ecd672deb927d
rotti <spmbot123@gmail.com>
2017-01-17 22:25:36 +0100

    create datasources ok

--- grava_frontend.py
+++ grava_frontend.py
@@ -5,51 +5,54 @@ import json
 session = requests.Session()
 headers = {'content-type': 'application/json;charset=UTF-8'}
 
+
 host = "localhost"
 user = "admin"
 password = "admin"
 
+
 grafana_port = ":3000"
 grafana_url = "http://" + host + grafana_port
+grafana_login_url = "http://" + host + grafana_port + "/login"
 grafana_auth_url = "http://" + user + ":" + password + "@" + host + grafana_port
-print "XXX", grafana_auth_url
 
-ifdb_database = "test"
+
+api_url_datasources = grafana_auth_url + "/api/datasources"
+
+
+ifdb_database = "strava"
 ifdb_port = ":8086"
 ifdb_url = "http://" + host + ifdb_port
-
-datasource_name = "XXX xxx"
-datasource_api = grafana_auth_url + "/api/datasources"
+datasource_name = "Grava Source"
 
 ifdb_source = {
-    'access': 'direct',
+    'access': 'proxy',
     'database': ifdb_database,
     'name': datasource_name,
     'password': password,
     'type': 'influxdb',
     'url': ifdb_url,
+    'isDefault': True,
     'user': user
 }
 
+############## Do stuff here ##########################
 
-# XXX write like POST
-login_post = session.post(
-   os.path.join(grafana_url, 'login'),
-   data=json.dumps({
-      'user': user,
-      'password': password }),
-   headers={'content-type': 'application/json'})
+do_login = session.post(url=grafana_login_url, data=json.dumps({'user': user, 'password': password}), headers=headers)
 
-datasources_get = session.get(os.path.join(grafana_url, 'api', 'datasources'))
-datasources = datasources_get.json()
-print "...get datasources", datasources
-
-print "XXX", grafana_auth_url
-create_datasource = session.post(url=datasource_api, data=json.dumps(ifdb_source), headers=headers)
+print "...looking for existing datasources in", grafana_url
+get_datasources = session.get(url=api_url_datasources)
+datasources = get_datasources.json()
 
 
+if not datasources:
+    post_datasource = session.post(url=api_url_datasources, data=json.dumps(ifdb_source), headers=headers)
+    print "...create datasource. using config", ifdb_source
+else:
+    print "...existing datasource(s) found."
+    print "...skipping create datasource."
 
-#make_datasource = session.post("http://admin:admin@localhost:3000/api/datasources", data=json.dumps(source), headers=headers)
+
 
 #curl 'http://admin:admin@192.168.99.100:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"localGraphite","type":"graphite","url":"http://192.168.99.100","access":"proxy","isDefault":true,"database":"asd"}'
 
